cca58ede28812cf65bffb2112a6d00f50948bfe6
Ranjith <ranjithsundar10@rediffmail.com>
2023-04-13 06:32:24 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import React, { useState, useTransition } from "react";
+  
+const defaultList = [1, 2, 3, 4, 5];
+  
+const App=()=> {
+    const [name, setName] = useState("");
+      const [list, setList] = useState(defaultList);
+      const [isPending, startTransition] = useTransition();
+      const sizeOfList = 10000;
+  
+      const handleChange=(e)=> {
+        const { value } = e.target;
+        setName(value);
+  
+    startTransition(() => {
+        const data = [];
+        for (let i = 0; i < sizeOfList; i++) {
+            data[i] = value;
+        }
+        setList(data);
+       });
 }
+  
+    return (
+        <>
+              <input type="text" value={name} onChange={handleChange} />
+              {isPending ? (
+                <div>Loading...</div>
+             ) : (
+                   list.map((list) => {
+                      return <div>List Item: {list}</div>;
+                })
+              )}
+            </>
+    );
+}
+  
+export default App;
